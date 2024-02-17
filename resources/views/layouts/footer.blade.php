ed0cceb971dbc7bb0138fa03a6f7ff5994f909b4
aps1pos <nanthagopalmarichamy@gmail.com>
2024-02-17 16:54:07 +0530

    test commot

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="c-footer">
+    <div>Aps Pos © {{ date('Y') }} || Developed by <strong><a target="_blank" href="https://aps-infotech-solutions.ueniweb.com/">Aps InfoTech Software Solution , Erichanatham , JohnsonAPS</a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
